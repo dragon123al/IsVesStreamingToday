d82cf44e64b65c09413430c0f8f084d0ccdcfdd6
dragon123al <129787161+dragon123al@users.noreply.github.com>
2025-08-29 21:27:38 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const currentDate = new Date();
+const dayIndex = currentDate.getDay();
+// const testDayIndex = 0;
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const today = daysOfWeek[dayIndex];
+
+const streamToday = (day) => {
+  return day === "Monday" || day === "Tuesday" || day === "Thursday";
+};
+
+const statusElement = document.getElementById("stream-status");
+
+const streamLink = "https://www.twitch.tv/vespernoctum";
+
+if (streamToday(today)) {
+  statusElement.innerHTML = `<a href="${streamLink}" target="_blank">Yes</a>`;
+} else {
+  statusElement.textContent = "No.";
+}
+
